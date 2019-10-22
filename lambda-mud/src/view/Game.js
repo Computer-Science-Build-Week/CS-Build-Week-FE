9036651caf777e14d7970c6460a3a1b416a7326e
Sorin Chis <kish.sorin@yahoo.com>
2019-10-22 17:10:47 +0300

    added costum axios function for  setting the header

--- lambda-mud/src/view/Game.js
+++ lambda-mud/src/view/Game.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
+import Controls from "./Controls";
+import axios from "axios";
 
 const Game = ({ history }) => {
+  const [details, setDetails] = useState({});
+  const [direction, setDirection] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://lambda-mud-test.herokuapp.com/api/adv/init/")
+      .then(res => {
+        console.log(res);
+        setDetails(res.data);
+      });
+  }, []);
+
   return (
     <>
       <h1>Game Plan/Dashboard</h1>
@@ -13,6 +28,7 @@ const Game = ({ history }) => {
       >
         Logout
       </button>
+      <Controls />;
     </>
   );
 };
